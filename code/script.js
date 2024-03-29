7976cecacd32ee0db3f0bc49fb1f1003d4a50b71
Sidi Chaikh <98885777+sidi34308@users.noreply.github.com>
2024-03-29 22:30:51 +0300

    code

--- code/script.js
+++ code/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     // Functionality to toggle cart visibility
     const body = document.querySelector('body');
@@ -44,6 +45,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const http_users = new XMLHttpRequest();
     let users;
 
+<<<<<<< Updated upstream
     http_users.open('GET', 'users.json', true);
     http_users.send();
 
@@ -211,6 +213,186 @@ document.addEventListener("DOMContentLoaded", function() {
                             </div>
                         </div>`;
                 }
+=======
+const retrievedUser = localStorage.getItem('logged_in_user');
+console.log("loca",retrievedUser);
+if (retrievedUser) {
+            logged_in_user = JSON.parse(retrievedUser);
+            console.log(logged_in_user);
+            console.log("Logined in user is",logged_in_user.name);
+            const userPopup = document.getElementById("userPopup");
+            const userimage = document.createElement("img");
+            console.log(logged_in_user.image);
+            userimage.src = `${logged_in_user.image}`;
+            const userName = document.createElement("h3");
+            userName.textContent = logged_in_user.name;
+            
+            const balance = document.createElement("p");
+            balance.textContent = "Balance: " + logged_in_user.balance + " QR";
+
+
+           const signOutButton = document.createElement("button");
+            signOutButton.textContent = "Sign Out";
+             
+                userPopup.innerHTML = ""; // Clear previous content
+                userPopup.appendChild(userimage);
+                userPopup.appendChild(userName);
+                userPopup.appendChild(balance);
+                userPopup.appendChild(signOutButton);
+                signOutButton.addEventListener("click", () => {
+                    logged_in_user=[];
+                    console.log(logged_in_user);
+                    console.log("you are logged-out");
+                    const userPopup = document.getElementById("userPopup");
+    
+                    const userimage = document.createElement("img");
+                    userimage.src = "media/icons/account-black.png";
+    
+                    const userName = document.createElement("h3");
+                    userName.textContent = "  ";
+
+                    const balance = document.createElement("p");
+                    balance.textContent = "log in to see your info";
+
+                    const LogInButton = document.createElement("button");
+                    LogInButton.textContent = "Sign in";
+                
+                    LogInButton.addEventListener("click", () =>{togglePopup() });
+                    console.log("Signing out...");
+                    userPopup.innerHTML = ""; // Clear previous content
+                    userPopup.appendChild(userimage);
+                    userPopup.appendChild(userName);
+                    userPopup.appendChild(balance);
+                    userPopup.appendChild(LogInButton);
+                    localStorage.setItem('logged_in_user', JSON.stringify(logged_in_user=[]));
+                });
+}
+
+
+window.addEventListener('load', function() {
+    function handleLogin(users) {
+        // Get the input values
+        var username = document.querySelector('.login-input[name="uname"]').value;
+        var password = document.querySelector('.login-input[name="psw"]').value;
+        
+        let found = false;
+
+        // Find the user with the matching username and password
+        for (let user of users) {
+            if (user.name === username && user.password === password) {
+                found = true;
+                
+                logged_in_user=user;
+                console.log(logged_in_user);
+                console.log("Logined in user is",logged_in_user.name);
+                const userPopup = document.getElementById("userPopup");
+
+                const userimage = document.createElement("img");
+                console.log(logged_in_user.image);
+                userimage.src = `${logged_in_user.image}`;
+
+                const userName = document.createElement("h3");
+                userName.textContent = logged_in_user.name;
+            
+                const balance = document.createElement("p");
+                balance.textContent = "Balance: " + logged_in_user.balance + " QR";
+
+
+                const signOutButton = document.createElement("button");
+                signOutButton.textContent = "Sign Out";
+             
+                signOutButton.addEventListener("click", () => {
+                    logged_in_user=[];
+                    console.log(logged_in_user);
+                    console.log("you are logged-out");
+                    const userPopup = document.getElementById("userPopup");
+    
+                    const userimage = document.createElement("img");
+                    userimage.src = "media/icons/account-black.png";
+    
+                    const userName = document.createElement("h3");
+                    userName.textContent = "  ";
+
+                    const balance = document.createElement("p");
+                    balance.textContent = "sign in to see your info";
+
+                    const LogInButton = document.createElement("button");
+                    LogInButton.textContent = "Sign in";
+                
+                    
+                    console.log("Signing out...");
+                    userPopup.innerHTML = ""; // Clear previous content
+                    userPopup.appendChild(userimage);
+                    userPopup.appendChild(userName);
+                    userPopup.appendChild(balance);
+                    userPopup.appendChild(LogInButton);
+                    LogInButton.addEventListener("click", () =>{togglePopup() });
+                    localStorage.setItem('logged_in_user', JSON.stringify(logged_in_user));
+                });
+            
+
+                // Create a sign-out button
+              
+                userPopup.innerHTML = ""; // Clear previous content
+                userPopup.appendChild(userimage);
+                userPopup.appendChild(userName);
+                userPopup.appendChild(balance);
+                userPopup.appendChild(signOutButton);
+            // Append user info and sign-out button to the user popup
+         
+                break; // No need to continue searching once user is found
+                
+            }
+        }
+
+        if (found) {
+            // Login successful
+            console.log('Login successful! Welcome, ' + username + '!');
+            togglePopup(); 
+            localStorage.setItem('logged_in_user', JSON.stringify(logged_in_user));
+
+            // You can redirect or perform any other action here
+        } else {
+            // Login failed
+            console.log('Invalid username or password. Please try again.');
+        }
+       
+    }
+
+    document.querySelector(".login-button").addEventListener("click", () => {
+        handleLogin(users);
+        console.log("Logined in user is",logged_in_user.name);
+       
+    });
+
+    // Functionality to fetch and display products
+    const http_products = new XMLHttpRequest();
+    http_products.open('GET', 'products.json', true);
+    http_products.send();
+
+    http_products.onload = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const products = JSON.parse(this.responseText);
+            let output = '';
+
+            for (const item of products) {
+                output += `
+                    <div class="card ">
+                        <div class="image-container">
+                            <img src="${item.image}" alt="Product Image">
+                        </div>
+                        <div class="details">
+                            <div class="product-name">${item.productname}</div>
+                            <div class="price">${item.price} QR</div>
+                        </div>
+                        <div class="addCart" data-id="${item.id}">
+                            <button>Add to cart</button>
+                        </div>
+                    </div>`;
+            }
+
+            document.querySelector(".cards-container").innerHTML = output;
+>>>>>>> Stashed changes
 
                 document.querySelector(".cards-container").innerHTML = output;
 
@@ -370,7 +552,17 @@ document.addEventListener("DOMContentLoaded", function() {
                     
                 };
                
+<<<<<<< Updated upstream
             }
         };
     });
+=======
+                refreshTotalprice(cart);
+                totalcart(cart);
+                
+            };
+           
+        }
+    };
+>>>>>>> Stashed changes
 });
